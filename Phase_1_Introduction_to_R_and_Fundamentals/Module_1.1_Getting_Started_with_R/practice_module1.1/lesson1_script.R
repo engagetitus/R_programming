551a5632ea31f35156c52e2b35f99de719fc1095
titusmuthomi <90460893+titusmuthomi@users.noreply.github.com>
2025-05-27 13:06:23 +0300

    lesson 1 updates & practice

---
+++ Phase_1_Introduction_to_R_and_Fundamentals/Module_1.1_Getting_Started_with_R/practice_module1.1/lesson1_script.R
@@ -0,0 +1,13 @@
+# This is my first R script!
+
+# Calculate the area of a rectangle
+length <- 15
+width <- 7
+area <- length * width
+print(area)
+
+# Calculate the circumference of a circle
+radius <- 5
+pi_value <- 3.14159
+circumference <- 2 * pi_value * radius
+print(circumference)
